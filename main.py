4fa796861da00dbbd653379df93f94a44e0ef41d
Calebe Lino <calebe.n.lino@gmail.com>
2022-04-13 18:01:19 -0300

    Implemented thresholding

--- main.py
+++ main.py
@@ -2,5 +2,7 @@ import numpy as np
 import cv2
 
 static0 = cv2.imread("fruits00.jpg")
-cv2.imshow("fruits", static0)
+static0_grey = cv2.cvtColor(static0, cv2.COLOR_RGB2GRAY)
+ret, thresh = cv2.threshold(static0_grey, 100, 255, 0)
+cv2.imshow("fruits", thresh)
 cv2.waitKey(0)
